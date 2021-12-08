aa344cc62ed96a85b42223b420548d41402e5b56
Yuuya Watabe <watabe951@gmail.com>
2021-12-08 11:47:41 +0900

    add useful message as PID

--- execbg/__main__.py
+++ execbg/__main__.py
@@ -1,10 +1,19 @@
 import subprocess
 import sys
+import argparse
 
 def main():
-    executable = sys.argv
-    del executable[0]
-    print("run:", executable)
-    subprocess.Popen(executable, creationflags=subprocess.CREATE_NO_WINDOW, close_fds=True)
+    # parse
+    parser = argparse.ArgumentParser(description='run command in background')
+    parser.add_argument('command', type=str, nargs=1, help='command to execute')
+    parser.add_argument('arg', type=str, nargs='*', help='arguments to pass to the command')
+    args = parser.parse_args()
+
+    executable = args.command + args.arg
+    # run
+    po = subprocess.Popen(executable, creationflags=subprocess.CREATE_NO_WINDOW, close_fds=True)
+
+    # send some message
+    print(f"run: {' '.join(executable)}, PID: {po.pid}")
 if __name__ == "__main__":
     main()
